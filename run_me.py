f64b50c73da44e9b7987691f759228320ccff433
Sakib <sakibkuet16@gmail.com>
2022-06-16 01:39:24 +0600

    sdaf

--- run_me.py
+++ run_me.py
@@ -1,23 +1,33 @@
 import os 
 import pandas as pd
 
-query = "Garden Center"
+def Get_links():
+    query = "Garden Center"
 
-query_string = "+".join(query.split()).strip()
-df = pd.read_csv('states.csv')
+    query_string = "+".join(query.split()).strip()
+    df = pd.read_csv('states.csv')
 
-for i in range(2,len(df)):
-    state = df.loc[i,'State']
-    print(state)
-    command = f"scrapy crawl yp -o output_links/{i}-{state.replace(' ', '_')}.csv -a query={query_string} -a location={state.replace(' ','+')}"
-    os.system(command)
+    for i in range(0,len(df)):
+        state = df.loc[i,'State']
+        print(state)
+        command = f"scrapy crawl yp -o output_links/{i}-{state.replace(' ', '_')}.csv -a query={query_string} -a location={state.replace(' ','+')}"
+        os.system(command)
 
-files = os.listdir('output_links/')
-files.sort()
+def Get_Data():
+    files = os.listdir('output_links/')
+    files.sort()
 
-for file in files:
-    command = f"scrapy crawl yp_scraper -a input_file=output_links/{file} -o output_data/{file.replace('.csv','_data.csv')}"
-    os.system(command)
+    for file in files:
+        command = f"scrapy crawl yp_scraper -a input_file=output_links/{file} -o output_data/{file.replace('.csv','_data.csv')}"
+        os.system(command)
+
+def main():
+    Get_links()
+    Get_Data()
+
+
+if __name__ == '__main__':
+    main()
 
 
 
